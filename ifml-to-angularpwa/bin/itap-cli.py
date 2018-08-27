c069b3dd773d28ed087248ca8441c416d008f498
Fadh Lie <thedemonsays@gmail.com>
2018-08-27 18:31:37 +0700

    Basic Module Structure

--- ifml-to-angularpwa/bin/itap-cli.py
+++ ifml-to-angularpwa/bin/itap-cli.py
@@ -2,18 +2,17 @@ import click
 
 
 @click.group()
-def greet():
-    pass
+@click.option('--debug', default=False, help='Activating Debug Log (Default is False)')
+def main(debug):
+    click.echo('Debug is {debug}'.format(debug=debug))
 
 
-@greet.command()
-def hello(**kwargs):
-    click.echo('Hello')
-
-
-@greet.command()
-def goodbye(**kwargs):
-    click.echo('Goodbye')
+@main.command()
+@click.argument('ifml', type=click.Path(exists=True))
+@click.argument('class_diagram', type=click.Path(exists=True))
+#@click.option('--target-directory', default='', help='Target Directory to generate the Angular PWA Project')
+def generate(ifml, class_diagram):
+    click.echo('IFML model is: '+click.format_filename(ifml))
 
 if __name__ == '__main__':
-    greet()
+    main()
