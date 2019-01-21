875a2e2446c2c6044dd5097a358d89c419117770
Hafiyyan <hafiyyan94@gmail.com>
2019-01-21 13:47:56 +0700

    Requirement .txt

--- test/example_1/__init__.py
+++ test/example_1/__init__.py
@@ -4,4 +4,4 @@ from ifml_parser.ifmlxmiparser import parse
 from custom_xmi_parser.xmiparser_2 import parse as uml_parse
 logging.basicConfig(level=logging.DEBUG)
 
-generate_project('abs_bankaccount_withfee.core','abs_bankaccount.uml', target_directory="D:\\results", enable_login=True)
+generate_project('abs_bankaccount.core','abs_bankaccount.uml', target_directory="D:\\results", enable_login=True)
