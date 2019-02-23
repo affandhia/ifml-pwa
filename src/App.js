cf7a1066e5bc19b5d64a4fa2b9c533d128312480
Affan <affandhia@gmail.com>
2019-02-23 17:27:44 +0700

    remove unused code.

--- src/App.js
+++ src/App.js
@@ -17,46 +17,6 @@ const Home = () => (
   </div>
 )
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic}/>
-    <Route exact path={match.path} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
-
 const App = () => (
   <Router>
     <div>
@@ -67,8 +27,6 @@ const App = () => (
       <Route exact path="/" component={Home}/>
       <Route path="/customer" component={CustomerPage}/>
       <Route path="/account" component={ListAccountPage}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
     </div>
   </Router>
 )
