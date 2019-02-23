e35d8d60eb7e989b9933c6fc5eaa579dc459befb
Affan <affandhia@gmail.com>
2019-02-23 21:26:52 +0700

    adjust navbar to adopt auth mode

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
-    render() {
-        return (
-            <ul>
-                <li><Link to="/customer">Customer</Link></li>
-                <li><Link to="/account">Account</Link></li>
-            </ul>
-        )
+  onLogoutClicked = e => {
+    e.preventDefault();
+    this.props.logout();
+  };
+
+  renderMenu = () => {
+    if (!this.props.isAuth) return null;
+
+    return (
+      <React.Fragment>
+        <li>
+          <Link to="/customer">Customer</Link>
+        </li>
+        <li>
+          <Link to="/account">Account</Link>
+        </li>
+      </React.Fragment>
+    );
+  };
+
+  renderAuthMenu = () => {
+    if (this.props.isAuth) {
+      return (
+        <li>
+          <a href="/" onClick={this.onLogoutClicked}>
+            Logout
+          </a>
+        </li>
+      );
     }
+
+    return (
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    );
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.renderMenu()}
+        {this.renderAuthMenu()}
+      </ul>
+    );
+  }
 }
 
 export default Navbar;
