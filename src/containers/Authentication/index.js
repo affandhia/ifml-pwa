43c791741175fc3b064a7469147a7643d41deb3c
Affan <affandhia@gmail.com>
2019-02-25 16:14:43 +0700

    refactor relogin after refresh

--- src/containers/Authentication/index.js
+++ src/containers/Authentication/index.js
@@ -16,64 +16,73 @@ export class AuthProvider extends React.Component {
   tokenManager = new Token();
 
   componentDidMount = () => {
-    const token = this.tokenManager.get();
-    if (!token) {
-      this.logout();
-      return;
-    }
-
     const googleLoadTimer = setInterval(() => {
       this.setAuthLoadingStatus(LOADING_STATUS.INITIAL);
       if (window.gapi) {
         this.setAuthLoadingStatus(LOADING_STATUS.LOADING);
-        this.initGoogle(googleAuth => {
+        this.loadGoogleAPI(() => {
           clearInterval(googleLoadTimer);
-          this.checkGoogle(googleAuth);
+          this.setAuthLoadingStatus(
+            LOADING_STATUS.LOADED,
+            this.loadAuth2Success
+          );
         });
       }
     }, 90);
   };
 
-  componentWillUpdate = () => {
+  componentWillUpdate = async () => {
     console.log(this.state.isAuth);
   };
 
   // GOOGLE Method
 
-  initGoogle = func => {
-    window.gapi.load('auth2', function() {
-      window.gapi.auth2
-        .init({
-          client_id: process.env.REACT_APP_GOOGLE_CLIENTID,
-        })
-        .then(func);
+  loadAuth2Success = async () => {
+    const token = this.tokenManager.get();
+
+    if (!token) {
+      this.logout();
+    } else {
+      this.checkGoogle(token);
+    }
+  };
+
+  loadGoogleAPI = func => {
+    window.gapi.load('auth2', () => {
+      func();
+    });
+  };
+
+  initializeGoogle = () => {
+    return window.gapi.auth2.init({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENTID,
     });
   };
 
   getGoogleAuth = () => {};
 
-  checkGoogle = googleAuth => {
+  checkGoogle = async token => {
+    const googleAuth = await this.initializeGoogle();
+
     if (googleAuth.isSignedIn.get()) {
       const googleUser = googleAuth.currentUser.get();
       const tokenId = googleUser.getAuthResponse().id_token;
-      this.login(tokenId, () => {
-        this.setState({ googleAuth }, () =>
-          this.setAuthLoadingStatus(LOADING_STATUS.LOADED)
-        );
-      });
-      return;
-    }
 
-    this.setState({ googleAuth }, () =>
-      this.setAuthLoadingStatus(LOADING_STATUS.LOADED)
-    );
+      if (token === tokenId) {
+        this.login(tokenId, () => {
+          this.setState({ googleAuth });
+        });
+      } else {
+        this.logout();
+      }
+    }
   };
 
-  loginGoogle = () => {
-    const googleAuth = this.state.googleAuth;
-    if (!googleAuth) {
-      this.initGoogle(this.loginGoogleHelper);
-      return;
+  loginGoogle = async () => {
+    let googleAuth = this.state.googleAuth;
+
+    if (this.state.googleMethod === LOADING_STATUS.LOADED) {
+      googleAuth = await this.initializeGoogle();
     }
 
     this.loginGoogleHelper(googleAuth);
@@ -91,16 +100,14 @@ export class AuthProvider extends React.Component {
       });
   };
 
-  logoutGoogle = () => {
-    const googleAuth = this.state.googleAuth;
-    if (!googleAuth) {
-      this.initGoogle(auth => {
-        this.setAuthLoadingStatus(LOADING_STATUS.LOADED);
-        auth.signOut();
-      });
-    } else {
-      googleAuth.signOut();
+  logoutGoogle = async () => {
+    let googleAuth = this.state.googleAuth;
+
+    if (this.state.googleMethod === LOADING_STATUS.LOADED) {
+      googleAuth = await this.initializeGoogle();
     }
+
+    googleAuth.signOut();
   };
 
   // [END] GOOGLE Method
@@ -126,8 +133,8 @@ export class AuthProvider extends React.Component {
     this.setState({ isAuth: false });
   };
 
-  setAuthLoadingStatus = status => {
-    this.setState({ googleMethod: status });
+  setAuthLoadingStatus = (status, callback) => {
+    this.setState({ googleMethod: status }, callback);
   };
 
   render() {
