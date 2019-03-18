99092af21ee89f72185a064a3404ce2c73cebb6a
Affan <affandhia@gmail.com>
2019-03-18 16:40:25 +0700

    remove unused imported module

--- main/utils/jinja/language_template_writer.py
+++ main/utils/jinja/language_template_writer.py
@@ -1,4 +1,4 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader
 from main.utils.file_and_folder_management import path_joiner
 
 typescript_env = Environment(
