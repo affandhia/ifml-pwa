1f84615d85f60af6ee43e0d84dd5e8c35f88021f
Affan <affandhia@gmail.com>
2019-04-08 12:48:19 +0700

    ad setup proxy example file and fix react_structure

--- main/template/project_structure/react_structure.py
+++ main/template/project_structure/react_structure.py
@@ -6,7 +6,7 @@
 from main.utils.jinja.angular import base_file_writer
 
 default_structure = {
-    "README.md": "main/template/file/react/assets",
+    "README.md": "",
     "package.json": "",
     "public": {
         "favicon.ico": "main/template/file/react/assets/public",
@@ -14,7 +14,6 @@ default_structure = {
         "manifest.json": "main/template/file/react/assets/public"
     },
     "src": {
-        "App.css": "",
         "App.js": "",
         # "components": {
         #     "Form": {
@@ -52,11 +51,10 @@ default_structure = {
         #         "index.js": ""
         #     }
         # },
-        "index.css": "",
         "index.js": "",
         "logo.svg": "main/template/file/react/assets/src",
         "serviceWorker.js": "main/template/file/react/assets/src",
-        "setupProxy.js": "",
+        "setupProxy.js.example": "main/template/file/react/assets/src",
         "utils": {
             "token.js": ""
         }
