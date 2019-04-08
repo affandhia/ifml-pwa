bebd32357d4453dec2df429cd9df16077de323a0
Affan <affandhia@gmail.com>
2019-04-08 12:46:35 +0700

    enable color code console logger.

--- test/example_1_react/__init__.py
+++ test/example_1_react/__init__.py
@@ -1,11 +1,15 @@
 import sys
 import logging
+import coloredlogs
+
+logger_level = logging.DEBUG
+
+logging.basicConfig(stream=sys.stdout, level=logger_level)
+coloredlogs.install(level=logging._checkLevel(logger_level))
 
 sys.path.append('/Users/affandhia/Documents/Affan/skripsi/ifml-angular-pwa')
 
 from main.core.react.main import generate_project
-
-logging.basicConfig(level=logging.DEBUG)
 
 generate_project('abs_bankaccount.core', 'abs_bankaccount.uml',
                  target_directory="./result", enable_login=True,
