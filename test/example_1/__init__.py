d775bc1f37dc3bf3e0e702863948d513e24a1bd2
Affan <affandhia@gmail.com>
2019-04-30 23:04:28 +0700

    update git ignore and remove tracked files.

--- test/example_1/__init__.py
+++ test/example_1/__init__.py
@@ -4,6 +4,11 @@ from main.core.angular.main import generate_project
 import logging
 from ifml_parser.ifmlxmiparser import parse
 from custom_xmi_parser.xmiparser_2 import parse as uml_parse
-logging.basicConfig(level=logging.DEBUG)
+import coloredlogs
+
+logger_level = logging.DEBUG
+
+logging.basicConfig(stream=sys.stdout, level=logger_level)
+coloredlogs.install(level=logging._checkLevel(logger_level))
 
 generate_project('abs_bankaccount.core','abs_bankaccount.uml', target_directory="./result", enable_login=True)
--- test/example_1/result/abs-bankaccount/e2e/src/app.e2e-spec.ts
+++
@@ -1,7 +0,0 @@
-import {
-    AppPage
-} from './app.po';
-
-describe('workspace-project App', () => {
-    let page: AppPage;
-});
